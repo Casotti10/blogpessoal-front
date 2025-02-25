1745bc1a32fc4da5a07a925e351cfe58684c829f
Lucas <lucascasotti1@gmail.com>
2025-02-24 22:03:52 -0300

    Adicioanando Login e Cdastro

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -9,13 +9,21 @@ function Footer() {
             <div className="flex justify-center bg-indigo-900 text-white">
                 <div className="container flex flex-col items-center py-4">
                     <p className='text-xl font-bold'>
-                            Blog Pessoal Generation | Copyright: {data}
-                        </p>
+                        Blog Pessoal Generation | Copyright: {data}
+                    </p>
                     <p className='text-lg'>Acesse as minhas redes sociais</p>
                     <div className='flex gap-2'>
-                        <LinkedinLogo size={48} weight='bold' />
-                        <InstagramLogo size={48} weight='bold' />
-                        <FacebookLogo size={48} weight='bold' />
+                        <div className='flex gap-2'>
+                            <a href="https://www.linkedin.com/in/lucas-casotti-042716290/" target="_blank">
+                                <LinkedinLogo size={48} weight='bold' />
+                            </a>
+                            <a href="https://www.instagram.com/stories/_lucascasotti/" target="_blank">
+                                <InstagramLogo size={48} weight='bold' />
+                            </a>
+                            <a href="https://github.com/Casotti10" target="_blank">
+                                <FacebookLogo size={48} weight='bold' />
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
