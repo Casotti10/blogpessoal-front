f6b33f774410357620741c792ab35545d8c2e3e9
Lucas <lucascasotti1@gmail.com>
2025-02-25 08:35:10 -0300

    Service

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import './App.css'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
